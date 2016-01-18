8d74fc37144c30863ba18ef71bc773b7ccb418c1
Pawel Kowalski <kowalski.p@gmail.com>
2016-01-18 05:39:38 +0000

    added twbs:bootstrap package + some code modified for this too
    almost the end of week 1

--- image_share.js
+++ image_share.js
@@ -1,7 +1,23 @@
 if (Meteor.isClient) {
+  var img_data = [
+    {
+      img_src: "laptops.jpg",
+      img_alt: "some laptops on the table"
+    },
+    {
+      img_src: "bass.jpg",
+      img_alt: "a bass player"
+    },
+    {
+      img_src: "beard.jpg",
+      img_alt: "some some musicians with beards"
+    },
 
+  ];
+
+  Template.images.helpers({imagess:img_data});
 }
 
 if (Meteor.isServer) {
-
+  console.log("I'm the server");
 }
